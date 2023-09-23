717d989ec7c527b4b42329500d0a5616d44821a8
Amir <Amirhif123@hotmail.com>
2023-09-23 15:34:48 +0200

    first commit

---
+++ WebAWSApp/src/main/java/com/example/webawsapp/WebAwsAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.webawsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAwsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAwsAppApplication.class, args);
+    }
+
+}
---
+++ WebAWSApp/src/test/java/com/example/webawsapp/WebAwsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webawsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAwsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
